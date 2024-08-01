090f17173f4b7da162859e9635a32e57e0b5aace
Anton Sviridov <keynmol@gmail.com>
2024-08-01 18:55:27 +0100

    headers and stuff

--- modules/core/src/main/scala/Prompt.scala
+++ modules/core/src/main/scala/Prompt.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Anton Sviridov
+ * Copyright 2023 Neandertech
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
