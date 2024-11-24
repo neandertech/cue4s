da80a2982bdf215e57a84929b868fd1a0d0030fa
Anton Sviridov <keynmol@gmail.com>
2024-11-24 19:46:03 +0000

    Flush out/err aggressively
    I noticed it was causing wild problems on JVM -
    I think we overflow the buffer because we no longer send newlines,
    so it doesn't flush

--- modules/core/src/main/scala-jvm-native/PlatformStd.scala
+++ modules/core/src/main/scala-jvm-native/PlatformStd.scala
@@ -17,5 +17,9 @@
 package cue4s
 
 trait PlatformStd extends Output:
-  override def logLn[A: AsString](a: A): Unit = System.err.println(a.render)
-  override def out[A: AsString](a: A): Unit   = System.out.print(a.render)
+  override def logLn[A: AsString](a: A): Unit = 
+    System.err.println(a.render)
+    System.err.flush()
+  override def out[A: AsString](a: A): Unit   = 
+    System.out.print(a.render)
+    System.out.flush()
