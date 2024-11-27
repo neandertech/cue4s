9ac5ff85a5fcba51952ecfa621364fd1fd52672d
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-11-27 11:48:53 +0100

    retain position of cursor after search

--- modules/core/src/main/scala/InteractiveMultipleChoice.scala
+++ modules/core/src/main/scala/InteractiveMultipleChoice.scala
@@ -174,6 +174,9 @@ private[cue4s] object InteractiveMultipleChoice:
 
     override protected def scrollDown = copy(windowStart = windowStart + 1)
 
+    override protected def resetWindow() =
+      copy(windowStart = computeWindowStartAfterSearch)
+
     def addText(t: Char) =
       changeText(text + t).resetWindow()
 
@@ -187,9 +190,6 @@ private[cue4s] object InteractiveMultipleChoice:
           if selected(cursor) then copy(selected = selected - cursor)
           else copy(selected = selected + cursor)
 
-    override protected def resetWindow() =
-      copy(windowStart = 0)
-
     private def changeText(newText: String) =
       val newFiltered = all.filter((alt, _) =>
         newText.trim.isEmpty || alt
@@ -197,10 +197,20 @@ private[cue4s] object InteractiveMultipleChoice:
           .contains(newText.toLowerCase().trim())
       )
       if newFiltered.nonEmpty then
-        val newShowing = Some(
-          newFiltered.map(_._2) -> newFiltered.head._2
-        )
-        copy(text = newText, showing = newShowing)
+        showing match
+          case None =>
+            val newShowing = newFiltered.headOption.map: (_, id) =>
+              newFiltered.map(_._2) -> id
+
+            copy(text = newText, showing = newShowing)
+
+          case Some((_, selected)) =>
+            val newShowing = newFiltered.headOption.map: (_, id) =>
+              val newSelected =
+                newFiltered.find(_._2 == selected).map(_._2).getOrElse(id)
+              newFiltered.map(_._2) -> newSelected
+
+            copy(text = newText, showing = newShowing)
       else copy(showing = None, text = newText)
       end if
     end changeText
