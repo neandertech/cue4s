636c2f70598b356303ed8bd2e9608fa224417da4
Anton Sviridov <keynmol@gmail.com>
2025-11-10 09:50:02 +0000

    Fix cancellation on Windows

--- modules/core/src/main/scala-jvm-native/KeyboardReadingThread.scala
+++ modules/core/src/main/scala-jvm-native/KeyboardReadingThread.scala
@@ -6,7 +6,7 @@ private[cue4s] class KeyboardReadingThread[Result](
     whatNext: Next[Result] => Boolean,
     send: TerminalEvent => Boolean,
     getchar: () => Int,
-) extends Thread:
+) extends Thread("cue4s-keyboard-input-thread"):
   override def run(): Unit =
     var lastRead = 0
 
@@ -19,14 +19,17 @@ private[cue4s] class KeyboardReadingThread[Result](
 
     while !stop && read() >= 0 do
       if lastRead != 0 then
-        val (newState, result) = decode(state, lastRead)
+        try
+          val (newState, result) = decode(state, lastRead)
 
-        result match
-          case n: DecodeResult => stop = whatNext(n.toNext)
-          case e: TerminalEvent =>
-            stop = send(e)
+          result match
+            case n: DecodeResult => stop = whatNext(n.toNext)
+            case e: TerminalEvent =>
+              stop = send(e)
 
-        state = newState
+          state = newState
+        catch case ex: CharCollector.ExitThrowable.type =>
+          stop = whatNext(Next.Stop)
 
     end while
   end run
