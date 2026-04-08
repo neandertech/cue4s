063b1fb05026b8dc84e00193e0a8762a2165d4c2
Anton Sviridov <keynmol@gmail.com>
2026-04-08 09:37:47 +0100

    Make examples interactive

--- modules/example/src/main/scala-jvm-native/sync.scala
+++ modules/example/src/main/scala-jvm-native/sync.scala
@@ -19,69 +19,104 @@ package cue4s_example
 import cue4s.*
 import cue4s.Prompt.PasswordInput.Password
 
-@main def sync =
+@main def sync(which: String*) =
   Prompts.sync.use: prompts =>
-    val likeCats = prompts
-      .confirm("Do you like cats?")
-      .toOption
-
-    val day = prompts
-      .singleChoice(
-        "How was your day?",
-        List(
-          "amazing",
-          "productive",
-          "relaxing",
-          "stressful",
-          "exhausting",
-          "challenging",
-          "wonderful",
-          "uneventful",
-          "interesting",
-          "exciting",
-          "boring",
-          "demanding",
-          "satisfying",
-          "frustrating",
-          "peaceful",
-          "overwhelming",
-          "busy",
-          "calm",
-          "enjoyable",
-          "memorable",
-          "ordinary",
-          "fantastic",
-          "rewarding",
-          "chaotic",
-        ),
-        _.withWindowSize(7),
-      )
-      .toOption
-
-    val skyColor: Completion[Boolean] = prompts.run(
-      Prompt
-        .Input("What color is the sky?")
-        .mapValidated: s =>
-          Either.cond(s == "blue", true, PromptError("hint: it's blue")),
+    val examples = Map[String, (String, SyncPrompts => Unit)](
+      "confirm"       -> ("Yes/No confirmation", likeCats),
+      "single-choice" -> ("Single choice", day),
+      "multi-choice"  -> ("Multiple choice", letters),
+      "validated-int" -> ("Validated integer", seasons),
+      "password"      -> ("Password", password),
     )
 
-    val letters: Completion[List[String]] =
-      prompts.multiChoiceNoneSelected(
-        "What are your favourite letters?",
-        ('A' to 'Z').map(_.toString).toList,
-        _.withWindowSize(7),
-      )
+    if which.isEmpty then
+      val maxLength = examples.keys.map(_.length).max
 
-    val seasons: Completion[Int] =
-      prompts.int(
-        "How many seasons of Stargate SG-1 are there",
-        _.validate:
-          case x if x < 10 => Option(PromptError("More!"))
-          case x if x > 10 => Option(PromptError("Fewer!"))
-          case _           => None,
-      )
+      val selected = prompts
+        .multiChoiceNoneSelected(
+          "Which examples would you like to run?",
+          examples
+            .map { case (k, (desc, _)) =>
+              s"${k.padTo(maxLength, ' ')}: $desc"
+            }
+            .toList
+            .sorted,
+        )
+        .getOrThrow
 
-    val password: Completion[Password] =
-      prompts.password("Choose a new password")
+      val toShow = selected.map(_.split(": ").head.trim)
+      toShow.foreach { k => examples(k)._2(prompts) }
+    else
+      val allowed = examples.keySet
+      val missing = which.toSet -- allowed
+
+      if missing.nonEmpty then
+        sys.error(s"Unknown example names: ${missing
+            .mkString(", ")}. Available examples: ${allowed.mkString(", ")}")
+      else which.foreach { k => examples(k)._2(prompts) }
+    end if
 
 end sync
+
+def likeCats(prompts: SyncPrompts) = prompts
+  .confirm("Do you like cats?")
+  .toOption
+
+def day(prompts: SyncPrompts) = prompts
+  .singleChoice(
+    "How was your day?",
+    List(
+      "amazing",
+      "productive",
+      "relaxing",
+      "stressful",
+      "exhausting",
+      "challenging",
+      "wonderful",
+      "uneventful",
+      "interesting",
+      "exciting",
+      "boring",
+      "demanding",
+      "satisfying",
+      "frustrating",
+      "peaceful",
+      "overwhelming",
+      "busy",
+      "calm",
+      "enjoyable",
+      "memorable",
+      "ordinary",
+      "fantastic",
+      "rewarding",
+      "chaotic",
+    ),
+    _.withWindowSize(7),
+  )
+  .toOption
+
+def skyColor(prompts: SyncPrompts): Completion[Boolean] = prompts.run(
+  Prompt
+    .Input("What color is the sky?")
+    .mapValidated: s =>
+      Either.cond(s == "blue", true, PromptError("hint: it's blue")),
+)
+
+def letters(prompts: SyncPrompts): Completion[List[String]] =
+  prompts.multiChoiceNoneSelected(
+    "What are your favourite letters?",
+    ('A' to 'Z').map(_.toString).toList,
+    _.withWindowSize(7),
+  )
+
+def seasons(prompts: SyncPrompts): Completion[Int] =
+  prompts.int(
+    "How many seasons of Stargate SG-1 are there",
+    _.validate:
+      case x if x < 10 => Option(PromptError("More!"))
+      case x if x > 10 => Option(PromptError("Fewer!"))
+      case _           => None,
+  )
+
+def password(prompts: SyncPrompts): Completion[Password] =
+  prompts.password("Choose a new password")
